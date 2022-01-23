a520e62f7fc930d2808912828586cab6025ee2b8
yolan royan <yolan.royan@gmail.com>
2022-01-23 18:06:59 +0200

    trainer script now trains and computes metrics on test

--- starter/train_model.py
+++ starter/train_model.py
@@ -3,9 +3,12 @@
 from sklearn.model_selection import train_test_split
 
 # Add the necessary imports for the starter code.
+from ml.data import process_data
+from ml.model import train_model, compute_model_metrics, inference
 
 # Add code to load in the data.
-
+import pandas as pd 
+data = pd.read_csv('../data/census_a_nowhitespace.csv')
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
 
@@ -23,6 +26,13 @@ X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
 
-# Proces the test data with the process_data function.
+# Process the test data with the process_data function.
+X_test, y_test, _, _ = process_data(
+    test, categorical_features=cat_features, label="salary", training=False,
+    encoder=encoder, lb=lb
+)
 
 # Train and save a model.
+model = train_model(X_train, y_train)
+preds = inference(model, X_test)
+print(compute_model_metrics(preds, y_test))
