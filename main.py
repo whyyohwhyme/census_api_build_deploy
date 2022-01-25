eef7fb5584a7d456c30b80603d705896e32fb4a9
yolan royan <yolan.royan@gmail.com>
2022-01-25 23:09:16 +0200

    heroku-specific files

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 # Put the code for your API here.
+import os
+
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
+
 from fastapi import FastAPI
 from pydantic import BaseModel 
 from starter.ml.data import process_data
