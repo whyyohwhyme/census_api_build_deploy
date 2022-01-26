a53992f23035872c2409395e44ba2645c119bf0a
yolan royan <yolan.royan@gmail.com>
2022-01-26 09:52:55 +0200

    minor bugfixes

--- test_main.py
+++ test_main.py
@@ -5,67 +5,67 @@ client = TestClient(app)
 
 # df = pd.read_csv('data/census_a_nowhitespace.csv')
 # df.query('salary == ">50K"').sample(1).to_dict(orient='record')
-pos_examples = [{'age': 55
-                 'workclass': '?'
-                 'fnlgt': 141807
-                 'education': 'HS-grad'
-                 'education-num': 9
-                 'marital-status': 'Never-married'
-                 'occupation': '?'
-                 'relationship': 'Not-in-family'
-                 'race': 'White'
-                 'sex': 'Male'
-                 'capital-gain': 13550
-                 'capital-loss': 0
-                 'hours-per-week': 40
-                 'native-country': 'United-States'
+pos_examples = [{'age': 55,
+                 'workclass': '?',
+                 'fnlgt': 141807,
+                 'education': 'HS-grad',
+                 'education-num': 9,
+                 'marital-status': 'Never-married',
+                 'occupation': '?',
+                 'relationship': 'Not-in-family',
+                 'race': 'White',
+                 'sex': 'Male',
+                 'capital-gain': 13550,
+                 'capital-loss': 0,
+                 'hours-per-week': 40,
+                 'native-country': 'United-States',
                  'salary': '>50K'}
 
-                {'age': 35
-                 'workclass': 'Private'
-                 'fnlgt': 356238
-                 'education': 'Assoc-acdm'
-                 'education-num': 12
-                 'marital-status': 'Never-married'
-                 'occupation': 'Other-service'
-                 'relationship': 'Not-in-family'
-                 'race': 'White'
-                 'sex': 'Female'
-                 'capital-gain': 0
-                 'capital-loss': 0
-                 'hours-per-week': 80
-                 'native-country': 'United-States'
+                {'age': 35,
+                 'workclass': 'Private',
+                 'fnlgt': 356238,
+                 'education': 'Assoc-acdm',
+                 'education-num': 12,
+                 'marital-status': 'Never-married',
+                 'occupation': 'Other-service',
+                 'relationship': 'Not-in-family',
+                 'race': 'White',
+                 'sex': 'Female',
+                 'capital-gain': 0,
+                 'capital-loss': 0,
+                 'hours-per-week': 80,
+                 'native-country': 'United-States',
                  'salary': '>50K'}]
-neg_examples = [{'age': 21
-                 'workclass': 'Private'
-                 'fnlgt': 216181
-                 'education': 'Some-college'
-                 'education-num': 10
-                 'marital-status': 'Never-married'
-                 'occupation': 'Sales'
-                 'relationship': 'Own-child'
-                 'race': 'White'
-                 'sex': 'Male'
-                 'capital-gain': 0
-                 'capital-loss': 0
-                 'hours-per-week': 35
-                 'native-country': 'United-States'
+neg_examples = [{'age': 21,
+                 'workclass': 'Private',
+                 'fnlgt': 216181,
+                 'education': 'Some-college,'
+                 'education-num': 10,
+                 'marital-status': 'Never-married',
+                 'occupation': 'Sales',
+                 'relationship': 'Own-child',
+                 'race': 'White',
+                 'sex': 'Male',
+                 'capital-gain': 0,
+                 'capital-loss': 0,
+                 'hours-per-week': 35,
+                 'native-country': 'United-States',
                  'salary': '<=50K'}
 
-                {'age': 42
-                 'workclass': 'Local-gov'
-                 'fnlgt': 99554
-                 'education': 'Some-college'
-                 'education-num': 10
-                 'marital-status': 'Married-civ-spouse'
-                 'occupation': 'Transport-moving'
-                 'relationship': 'Husband'
-                 'race': 'White'
-                 'sex': 'Male'
-                 'capital-gain': 0
-                 'capital-loss': 0
-                 'hours-per-week': 40
-                 'native-country': 'United-States'
+                {'age': 42,
+                 'workclass': 'Local-gov,'
+                 'fnlgt': 99554,
+                 'education': 'Some-college',
+                 'education-num': 10,
+                 'marital-status': 'Married-civ-spo,use'
+                 'occupation': 'Transpo,rt-moving'
+                 'relationship': 'Husband',
+                 'race': 'White',
+                 'sex': 'Male',
+                 'capital-gain': 0,
+                 'capital-loss': 0,
+                 'hours-per-week': 40,
+                 'native-country': 'United-States',
                  'salary': '<=50K'}]
 
 
